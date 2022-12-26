9122a0f5aaa8b1fd1aa32abc927be4a8aaa79c5c
ivantabera <ivantabera19@gmail.com>
2022-12-26 16:15:00 -0600

    Se realizo la navegacion mediante NavLink y rutas dinamicas

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,7 +1,41 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 export const Menu = () => {
 
     return(
-        <h1>Menu</h1>
+        <nav>
+            <ul>
+                {
+                    routes.map(route => (
+                        <li>
+                            <NavLink
+                                style={({isActive}) => ({
+                                    color: isActive ? 'blue' : 'red'
+                                })}
+                                to={route.to}
+                            >
+                                {route.text}
+                            </NavLink>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
     )
 }
+
+const routes = [];
+routes.push(
+    {
+        to: '/',
+        text: 'Home'
+    },
+    {
+        to: '/blog',
+        text: 'Blog'
+    },
+    {
+        to: '/profile',
+        text: 'Profile'
+    }
+)
