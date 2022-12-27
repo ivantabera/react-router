a616bd83f2f72df6f89d69aa41a74e65977a8552
ivantabera <ivantabera19@gmail.com>
2022-12-27 14:02:27 -0600

    uso del hook useNavigate

--- src/blog/blogpost/BlogPost.js
+++ src/blog/blogpost/BlogPost.js
@@ -1,15 +1,21 @@
 import React from "react";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {blogData} from "../../data/blogData";
 
 export const BlogPost = () => {
+    const navigate = useNavigate();
     const {slug} = useParams();
 
     const findPost = blogData.find(post => post.slug === slug);
 
+    const onReturn = () => {
+        navigate('/blog')
+    }
+
     return(
         <>
             <h2>{findPost.title}</h2>
+            <button onClick={onReturn}>Volver a los post</button>
             <p>{findPost.content}</p>
             <p>{findPost.author}</p>
         </>
